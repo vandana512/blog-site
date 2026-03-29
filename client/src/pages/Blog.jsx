e6bd034928ffb33fd29417ca455afe97af490b14
Vandana <vandanaaa512@gmail.com>
2026-03-30 01:51:51 +0530

    blog page in making

--- client/src/pages/Blog.jsx
+++ client/src/pages/Blog.jsx
@@ -1,11 +1,36 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { useState } from 'react'
+import { useParams } from 'react-router-dom'
+import { assets, blog_data } from '../assets/assets'
+import Navbar from '../components/Navbar'
 
 const Blog = () => {
-  return (
-    <div>
-      blog
+  const {id}= useParams()
+
+  const [data, setData]= useState(null);
+
+  const fetchBlogData = async ()=>{
+    const data= blog_data.find(item => item._id ===id)
+    setData(data)
+  }
+
+  useEffect(()=>{
+    fetchBlogData()
+  }, [])
+
+  return data ? (
+    <div className='relative'>
+      <img src={assets.gradientBackground} alt="" 
+      className='absolute -top-50 -z-1 opacity-50'/>
+
+      <Navbar/>
+
+      <div>
+        
+      </div>
+      
     </div>
-  )
+  ) : <div>Loading...</div>
 }
 
 export default Blog
