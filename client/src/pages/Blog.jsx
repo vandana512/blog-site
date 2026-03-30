8ae3faa6a64f8e81cdf6eb44dd2cf087746c6848
Vandana <vandanaaa512@gmail.com>
2026-03-30 11:27:36 +0530

    formatted the layout of blog

--- client/src/pages/Blog.jsx
+++ client/src/pages/Blog.jsx
@@ -3,6 +3,7 @@ import { useState } from 'react'
 import { useParams } from 'react-router-dom'
 import { assets, blog_data } from '../assets/assets'
 import Navbar from '../components/Navbar'
+import Moment from 'moment'
 
 const Blog = () => {
   const {id}= useParams()
@@ -25,9 +26,30 @@ const Blog = () => {
 
       <Navbar/>
 
-      <div>
-        
-      </div>
+      <div className='text-center mt-20 text-gray-600'>
+      <p className='text-primary py-4 font-medium'>
+        Published on {Moment(data.createdAt).format('MMMM Do YYYY')}
+      </p>
+
+      <h1 className='text-2xl sm:text-5xl font-semibold max-w-2xl mx-auto text-gray-800'>
+        {data.title}
+      </h1>
+
+      <h2 className='my-5 max-w-lg truncate mx-auto'>
+        {data.subTitle}
+      </h2>
+
+      <p className='inline-block py-1 px-4 rounded-full mb-6 border text-sm border-primary/35 bg-primary/5 font-medium text-primary'>
+        Michael Brown
+      </p>
+    </div>
+
+    <div>
+      <img src={data.image} alt="" className='rounded-3xl mb-5' />
+      
+      <div className='rich-text max-w-3xl mx-auto'
+      dangerouslySetInnerHTML={{__html: data.description}}></div>
+    </div>
       
     </div>
   ) : <div>Loading...</div>
