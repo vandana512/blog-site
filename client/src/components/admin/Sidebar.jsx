d5b3ce2de2d33253317789f00ca42831c920f00a
Vandana <vandanaaa512@gmail.com>
2026-03-31 09:34:19 +0530

    login page completed

--- client/src/components/admin/Sidebar.jsx
+++ client/src/components/admin/Sidebar.jsx
@@ -28,7 +28,7 @@ const Sidebar = () => {
             className={({isActive}) => `flex items-center gap-3 py-3.5 px-3 md:px-9 md:min-w-64 cursor-pointer ${isActive && "bg-primary/10 border-r-4 border-primary"}`}
         >
         <img src={assets.list_icon} alt="" className='min-w-4 w-5'/>
-        <p className='hidden md:inline-block'>List blogs</p>
+        <p className='hidden md:inline-block'>Blog Lists</p>
         </NavLink>
 
         <NavLink 
