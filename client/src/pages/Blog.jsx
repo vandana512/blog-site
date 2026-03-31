67a50e8bd2c0b95250d46447b7748764859df105
Vandana <vandanaaa512@gmail.com>
2026-03-31 08:35:18 +0530

    loading splash screen added

--- client/src/pages/Blog.jsx
+++ client/src/pages/Blog.jsx
@@ -5,6 +5,7 @@ import { assets, blog_data, comments_data } from '../assets/assets'
 import Navbar from '../components/Navbar'
 import Moment from 'moment'
 import Footer from '../components/Footer'
+import Loader from '../components/Loader'
 
 const Blog = () => {
   const {id}= useParams()
@@ -139,7 +140,7 @@ const Blog = () => {
     <Footer/>
 
     </div>
-  ) : <div>Loading...</div>
+  ) :  <Loader/> 
 }
 
 export default Blog
