791370d4dfb1214b0e64854c002503368f5a2d90
Vandana <vandanaaa512@gmail.com>
2026-04-02 22:18:18 +0530

    dashboard completed

--- client/src/pages/admin/Dashboard.jsx
+++ client/src/pages/admin/Dashboard.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import { assets, dashboard_data } from '../../assets/assets'
+import BlogTableItem from '../../components/admin/BlogTableItem'
 
 const Dashboard = () => {
   
@@ -62,7 +63,31 @@ const Dashboard = () => {
           <img src={assets.dashboard_icon_4} alt="" />
           <p>Latest Blogs</p>
         </div>
-      </div>      
+      </div>    
+
+{/* table */}
+      <div 
+      className='relative max-w-4xl overflow-x-auto shadow rounded-lg scrollbar-hide bg-white'>
+      <table 
+      className='w-full text-sm text-gray-500'>
+        <thead 
+        className='text-xs text-gray-600 text-left uppercase'>
+          <tr>
+            <th scope='col' className='px-2 py-4 xl:px-6'>#</th>
+            <th scope='col' className='px-2 py-4'>Blog Title</th>
+            <th scope='col' className='px-2 py-4 max-sm:hidden'>Date</th>
+            <th scope='col' className='px-2 py-4 max-sm:hidden'>Status</th>
+            <th scope='col' className='px-2 py-4'>Actions</th>
+          </tr>
+        </thead>
+
+        <tbody>
+          {dashboardData.recentBlogs.map((blog, index) =>{
+            return <BlogTableItem key={blog._id} blog={blog} fetchBlogs={fetchDashboard} index={index+1} />
+          })}
+        </tbody>
+      </table>
+    </div> 
     </div>
   )
 }
