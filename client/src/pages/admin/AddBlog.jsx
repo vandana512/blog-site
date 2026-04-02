5f47341f3a6c758b334347e810bc7b6e187c6f22
Vandana <vandanaaa512@gmail.com>
2026-04-03 02:21:01 +0530

    add blogs pg made + installed quill

--- client/src/pages/admin/AddBlog.jsx
+++ client/src/pages/admin/AddBlog.jsx
@@ -1,10 +1,78 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { assets } from '../../assets/assets'
+
 
 const AddBlog = () => {
+
+  const [image, setImage]= useState(false)
+  const [title, setTitle]= useState('')
+  const [subTitle, setSubTitle]= useState('')
+  const [category, setCategory]= useState('Startup')
+  const [isPublised, setIsPublised]=useState(false)
+
+  const onSubmitHandler= async(e) => {
+    e.preventDefault();
+  }
+
+  const generateContent= async()=>{
+
+  }
+
   return (
-    <div>
-      
-    </div>
+    <form onSubmit={onSubmitHandler} className='flex-1 bg-blue-50/50 text-gray-600 h-full overflow-scroll'>
+      <div className='bg-white w-full max-w-3xl p-4 md:p-10 sm:m-10 shadow rounded'>
+        
+        <p>Upload thumbnail</p>
+
+        <label htmlFor="image">
+          <img 
+            src={!image ? assets.upload_area : URL.createObjectURL(image)} 
+            alt="" 
+            className='mt-2 h-16 rounded cursor-pointer' 
+          />
+          <input 
+            onChange={(e)=> setImage(e.target.files[0])}
+            type="file" 
+            id="image" 
+            hidden 
+            required 
+          />
+        </label>
+
+        <p className='mt-4'>Blog title</p>
+        <input 
+          type="text" 
+          placeholder='Type here' 
+          required 
+          className='w-full max-w-lg mt-2 p-2 border border-gray-300 outline-none rounded'
+          onChange={(e)=> setTitle(e.target.value)} 
+          value={title}
+        />
+
+        <p className='mt-4'>Sub title</p>
+        <input 
+          type="text" 
+          placeholder='Type here' 
+          required 
+          className='w-full max-w-lg mt-2 p-2 border border-gray-300 outline-none rounded'
+          onChange={(e)=> setSubTitle(e.target.value)} 
+          value={subTitle}
+        />
+
+        <p className='mt-4'>Blog Description</p>
+
+        <div className='max-w-lg h-74 pb-16 sm:pb-10 pt-2 relative'>
+          <button 
+            type='button' 
+            onClick={generateContent} 
+            className='absolute bottom-1 right-2 ml-2 text-xs text-white bg-black/70 px-4 py-1.5 rounded hover:underline cursor-pointer'
+          >
+            Generate with AI
+          </button>
+        </div>
+
+      </div>
+    </form>
   )
 }
 
