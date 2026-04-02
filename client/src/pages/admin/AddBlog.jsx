6bda37f1234d6eb2a3d43b9d65f08b6b93ecc894
Vandana <vandanaaa512@gmail.com>
2026-04-03 02:34:16 +0530

    frontend completed

--- client/src/pages/admin/AddBlog.jsx
+++ client/src/pages/admin/AddBlog.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useRef, useState } from 'react'
-import { assets } from '../../assets/assets'
+import { assets, blogCategories } from '../../assets/assets'
 import Quill from 'quill'
 
 
@@ -12,7 +12,7 @@ const AddBlog = () => {
   const [title, setTitle]= useState('')
   const [subTitle, setSubTitle]= useState('')
   const [category, setCategory]= useState('Startup')
-  const [isPublised, setIsPublised]=useState(false)
+  const [isPublished, setIsPublished]=useState(false)
 
   const onSubmitHandler= async(e) => {
     e.preventDefault();
@@ -81,6 +81,37 @@ const AddBlog = () => {
             Generate with AI
           </button>
         </div>
+
+        <p className='mt-4'>Blog category</p>
+
+        <select 
+          onChange={(e)=> setCategory(e.target.value)} 
+          name="category"
+          className='mt-2 px-3 py-2 border text-gray-500 border-gray-300 outline-none rounded'
+        >
+          <option value="">Select category</option>
+
+          {blogCategories.map((item, index)=> {
+            return <option key={index} value={item}>{item}</option>
+          })}
+        </select>
+
+        <div className='flex gap-2 mt-4'>
+        <p>Publish Now</p>
+        <input 
+          type="checkbox" 
+          checked={isPublished} 
+          className='scale-125 cursor-pointer' 
+          onChange={(e)=> setIsPublished(e.target.checked)}
+        />
+      </div>
+
+      <button 
+        type="submit" 
+        className='mt-8 w-40 h-10 bg-primary text-white rounded cursor-pointer text-sm'
+      >
+        Add Blog
+      </button>
 
       </div>
     </form>
