e48e2c53e9be5c9f031813f108bbc8c723fa7f52
Vandana <vandanaaa512@gmail.com>
2026-04-05 21:59:05 +0530

    db connected

---
+++ server/configs/db.js
@@ -0,0 +1,13 @@
+import mongoose from "mongoose";
+
+const connectDB = async () => {
+    try{
+        mongoose.connection.on('connected', ()=> console.log("database connected"))
+        await mongoose.connect(`${process.env.MONGODB_URI}/blog`)
+    }
+    catch(error){
+        console.log(error.message)
+    }
+}
+
+export default connectDB;
