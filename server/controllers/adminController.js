ed0e8fb2cf15fccdd54dedcbac4fd823f8795270
Vandana <vandanaaa512@gmail.com>
2026-04-09 20:37:28 +0530

    added more admin controller

--- server/controllers/adminController.js
+++ server/controllers/adminController.js
@@ -1,4 +1,6 @@
 import jwt from 'jsonwebtoken'
+import Blog from '../models/Blog';
+import Comment from '../models/Comment';
 
 export const adminLogin= async(req, res)=>{
     try{
@@ -20,6 +22,62 @@ export const adminLogin= async(req, res)=>{
             token
         })
 
+
+    }
+    catch(error){
+        res.json({success: false, message:error.message})
+    }
+    
+}
+
+export const getAllBlogsAdmin= async(req, res)=>{
+    try{
+        const blogs= (await Blog.find({})).sort({createdAt:-1});
+        res.status(201).json({
+            message: "all bogs shown",
+            blogs
+        })
+
+    }
+    catch(error){
+        res.json({success: false, message:error.message})
+    }
+    
+}
+
+export const getAllComments= async(req, res)=>{
+    try{
+        // .find({})se sa return krta hai
+        const comments= (await Comment.find({})).populate('blog').sort({createdAt:-1});
+        res.status(201).json({
+            message: "all comments shown",
+            comments
+        })
+
+    }
+    catch(error){
+        res.json({success: false, message:error.message})
+    }
+    
+}
+
+export const getDashboard= async(req, res)=>{
+    try{
+        const recentBlogs= (await Blog.find({})).toSorted({createdAt: -1}).limit(5);
+
+        const blogs= await Blog.countDocuments();
+        const comments= await Comment.countDocuments();
+
+        const drafts= await Blog.countDocuments({isPublished: fasle});
+
+        const dashboardData= {
+            blogs, comments, drafts, recentBlogs
+        }
+
+        res.status(201).json({
+            message: "data fetched",
+            dashboardData
+        })
 
     }
     catch(error){
