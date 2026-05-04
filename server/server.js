aa19e7df4393460d75c91c95753103d623bd9a40
Vandana <vandanaaa512@gmail.com>
2026-05-05 00:26:52 +0530

    changes for deployment

--- server/server.js
+++ server/server.js
@@ -2,23 +2,37 @@ import express from 'express'
 import 'dotenv/config'
 import cors from 'cors'
 import connectDB from './configs/db.js'
+
 import adminRouter from './routes/adminRoutes.js'
 import blogRouter from './routes/blogRoutes.js'
 
 const app = express()
 
-await connectDB()
-
-//middlewares
 app.use(cors())
 app.use(express.json())
 
-//routes
-app.get('/', (req, res)=>{
-    res.send("Api is working")
+app.get('/', (req, res) => {
+  res.send("API is working")
 })
-// /api/admin is the prefix used
+
 app.use('/api/admin', adminRouter)
 app.use('/api/blog', blogRouter)
 
-export default app
+const startServer = async () => {
+  try {
+    await connectDB()
+    console.log("DB Connected")
+
+    const PORT = process.env.PORT || 5000
+
+    app.listen(PORT, () => {
+      console.log(`Server running on port ${PORT}`)
+    })
+
+  } catch (error) {
+    console.error("❌ Server start failed:", error)
+    process.exit(1)
+  }
+}
+
+startServer()
